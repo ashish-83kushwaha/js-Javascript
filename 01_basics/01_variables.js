136882629902fdbe857a840262e89265ebedc102
ashish-83kushwaha <ashish831700@gmail.com>
2025-09-07 04:31:31 +0000

    datatype

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 144553
+let accountEmail = "ashish@google.com"
+var accountPassword = "12345"
+accountCity = "jaipur"
+
+
+accountEmail = "asdffj.com"
+accountPassword = "2121212"
+accountCity = "bengaluru"
+
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,20 @@
+"use strict"; //treat all js code as newer version
+
+ console.log(3+3)
+
+ console.log("Ashish")
+ 
+
+ let name = "ashish"
+ let age = 18
+ let isLoggedId = false  
+
+ //number => 2 to power 53
+ //bigint
+ //string => ""
+ //boolean => true/false
+ //null => standalone value
+ //undefined =>
+ //symbol => unique
+
+ console.log(typeof null);
