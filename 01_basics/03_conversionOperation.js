38f157d3dd3d5c2db99db5eb81c0c520fe2d7b55
ashish-83kushwaha <ashish831700@gmail.com>
2025-09-08 04:11:08 +0000

    comparision

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,11 +1,11 @@
-let score = "33"
+//let score = "33"
 
-console.log(typeof score);
-console.log(typeof score);
+//console.log(typeof score);
+//console.log(typeof score);
 
-let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+//let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//onsole.log(valueInNumber);
 
 
 //"33"=>33
@@ -13,17 +13,33 @@ console.log(valueInNumber);
 //true=>1;false=>0
 
 
-let isLoggedIn =1
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+//let isLoggedIn =1
+//let booleanIsLoggedIn = Boolean(isLoggedIn)
+//onsole.log(booleanIsLoggedIn);
 
 //1=>true; 0=>false
 //"" =>false
 //"ashish" =>true
 
-let someNumber=33
+//let someNumber=33
 
-let stringNumber = String(someNumber)
+//let stringNumber = String(someNumber)
 
-console.log(stringNumber);
-console.log(typeof stringNumber);
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+
+//*******************Operations *****************
+
+let valu =3
+let negValue =-valu
+//console.log(negValue);
+
+//console.log("1" + 2);
+//console.log(1 + "2");
+//console.log("1" + 2 + 2);
+//console.log(1 + 2 + "2");
+
+let gameCounter = 100
+++ gameCounter;
+console.log(gameCounter);
