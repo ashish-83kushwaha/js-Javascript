366a92f0f5cabdcd3b392af7a2a2b4a421683a9a
ashish-83kushwaha <ashish831700@gmail.com>
2025-09-09 03:57:10 +0000

    Datatypes-summary

---
+++ 01_basics/datatype-summary.js
@@ -0,0 +1,11 @@
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false 
+const outsideTemp = null
+let userEmaile;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
